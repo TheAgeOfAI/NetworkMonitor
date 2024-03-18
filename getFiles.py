ebb122e070702b11ee2f91c22c8c5e8b39e4a455
TheAgeOfAI <badella2000@gmail.com>
2024-03-18 20:40:03 +0530

    made files as comma separated values

--- getFiles.py
+++ getFiles.py
@@ -9,58 +9,75 @@ import time
 def filterAndAppend() -> None:
 
     # This pattern search for a line that contain 64 bytes from IP with time in ms
-    pattern = re.compile(r"(\d+)\s+bytes\s+from\s+.*\s+([(\d.)]+).*time=(\d+\.?\d*)\s+ms")
-    start = re.compile(r"^\(")
-    end = re.compile(r"^\)")
-    blockString = ''
-    blockStatus = False
+    # pattern = re.compile(r"(\d+)\s+bytes\s+from\s+.*\s+([(\d.)]+).*time=(\d+\.?\d*)\s+ms")
+    # start = re.compile(r"^\(")
+    # end = re.compile(r"^\)")
+    block_string = ''
+    block_status = False
     # We are using try block to check if file are available or not.([(\d\.)]+)
     try:
         # Opening file to read data.
         with open('ping.txt') as file:
             # Opening file to write data.
-            with open("result.txt", 'a') as opt:
+            # with open("result.txt", 'a') as opt:
                 # data = file.readlines()
                 # readlines() can also be used but time-consuming.
-                for line in file:
-                    # checking for pattern if available in the line.
-                    result = pattern.search(line)
-                    block = start.search(line)
-                    if block:
-                        blockStatus = True
-                    ends = end.search(line)
-                    if ends:
-                        blockStatus = False
-                        somethings(blockString)
-                        blockString = ''
-                    if blockStatus:
-                        if not block:
-                            blockString += line
-                    # if result:
-                    #     # Writing to file if pattern found in the line.
-                    #     opt.write(f'{result.group(1)}, {result.group(2)[1:-1]}, {result.group(3)}\n')
+            for line in file:
+                # checking for pattern if available in the line.
+                # result = pattern.search(line)
+                start = re.search(r"^\(", line)
+                if start:
+                    block_status = True
+                end = re.search(r"^\)", line)
+                if end:
+                    block_status = False
+                    site_ping_data(block_string)
+                    block_string = ''
+                if block_status:
+                    if not start:
+                        block_string += line
+                # if result:
+                #     # Writing to file if pattern found in the line.
+                #     opt.write(f'{result.group(1)}, {result.group(2)[1:-1]}, {result.group(3)}\n')
 
 
     except Exception as e:
         print("This Exception -> ", e, " <- occurred.")
 
-def somethings(data):
-    pingFilter = re.compile("time=(\d+\.*\d+)\s+\S+")
+
+def date_time_split(data):
+    temp = data.split('-')[0].split(':')
+    year = temp[0]
+    month = temp[1]
+    date = temp[2]
+    temp = data.split('-')[1].split(':')
+    hours = temp[0]
+    minutes = temp[1]
+    seconds = temp[2]
+    weekday = data.split('-')[2]
+    weekend = 1 if weekday == 0 or weekday == 6 else 0
+
+    return f'{year}, {month}, {date}, {hours}, {minutes}, {seconds}, {weekday}, {weekend}'
+
+
+def site_ping_data(data):
+    ping_filter = re.compile("time=(\d+\.*\d+)\s+\S+")
     data = data.split('\n')
-    timeData = data[0]
-    siteData = data[1].split()[1].split('.')[0]
+    time_data = date_time_split(data[0])
+
+    site_data = data[1].split()[1].split('.')[0]
     ipaddress = data[1].split()[2][1:-1]
 
     with open('results.txt', 'a') as fd:
 
         for line in data[2:]:
-            ping = pingFilter.search(line)
+            ping = ping_filter.search(line)
             if ping:
-                fd.write(f'{timeData} {siteData} {ipaddress} {ping.group(1)}\n')
+                fd.write(f'{time_data}, {site_data}, {ipaddress}, {ping.group(1)}\n')
 
 
 # This function will make ping calls and return append that output to another file
-def pinger() -> None:
+def ping_to_file() -> None:
 
     hosts = ['google', 'duckduckgo', 'example', 'brave']
     param = '-c' if platform.system().lower() == 'linux' else '-n'
