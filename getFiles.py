ea433321eafb546d57bfdd83a069b354f0d82c79
TheAgeOfAI <badella2000@gmail.com>
2024-03-20 09:10:06 +0530

    make test file as csv file.

--- getFiles.py
+++ getFiles.py
@@ -1,5 +1,6 @@
-import re
+import csv
 import platform
+import re
 import subprocess
 import time
 
@@ -9,14 +10,16 @@ class Networking:
     def __init__(self):
         self.block_string = ''
         self.block_status = False
-        self.ping_file = './ping.txt'
-        self.param = '-c' if platform.system().lower() == 'linux' else '-n'
+        self.depricated =  './data/depricated.csv'
+        self.ping_file = './data/ping.txt'
+        self.result_file = './data/result.csv'
         self.hosts = ['google', 'duckduckgo', 'example', 'brave']
-        self.result_file = './result.txt'
+        self.param = '-c' if platform.system().lower() == 'linux' else '-n'
         self.pattern = re.compile(r"(\d+)\s+bytes\s+from\s+.*\s+.*time=(\d+\.?\d*)\s+ms")
+        self.pattern_depricated = re.compile(r"(\d+)\s+bytes\s+from\s+.*\s+([(\d.)]+).*time=(\d+\.?\d*)\s+ms")
 
     # This method will filter file and append the filtered data to another file and returns None.
-    def filter_block(self) -> None:
+    def read_filter_block(self) -> None:
         # We are using try block to check if file are available or not.
         try:
             # Opening file to read data.
@@ -28,7 +31,7 @@ class Networking:
                     end = re.search(r"^\)", line)
                     if end:
                         self.block_status = False
-                        self.site_ping_data(self.block_string)
+                        self.extract_ping(self.block_string)
                         self.block_string = ''
                     if self.block_status:
                         if not start:
@@ -37,7 +40,7 @@ class Networking:
         except Exception as e:
             print("This Exception -> ", e, " <- occurred.")
 
-    def site_ping_data(self, data):
+    def extract_ping(self, data) -> None:
         # This pattern search for a line that contain 64 bytes from IP with time in ms
         # pattern = re.compile(r"(\d+)\s+bytes\s+from\s+.*\s+([(\d.)]+).*time=(\d+\.?\d*)\s+ms")
         try:
@@ -48,29 +51,44 @@ class Networking:
             ipaddress = data[1].split()[2][1:-1]
 
             try:
-                with open(self.result_file, 'a') as fd:
+                with open(f'./data/{site_data}.csv', 'a') as fd:
 
                     for line in data[2:]:
                         ping = self.pattern.search(line)
                         if ping:
-                            fd.write(f'{time_data}, {site_data}, {ipaddress}, {ping.group(2)}\n')
+                            fd.write(f'{time_data}, {site_data}, {ipaddress}, \{ping.group(2)}\n')
             except Exception as e:
                 print("Exception with file.", e)
 
         except Exception as e:
             print("Exception may be due to ping error in os level.", e)
 
+    def extract_ping_depricated(self) -> None:
+        try:
+            with open(self.ping_file) as fd:
+                with open(self.depricated,'a') as csvfile:
+                    file = csv.writer(csvfile, delimiter=',')
+                    for line in fd:
+                        ping = self.pattern_depricated.search(line)
+                        if ping:
+                            file.writerow([ping.group(2)[1:-1], ping.group(3)])
+                        
+        except Exception as e:
+            print("This Exception -> ", e, " <- occurred.")
+
     # This function will make ping calls and return append that output to another file
     def ping_to_file(self) -> None:
 
         sites = {}
+        processes = {}
         try:
-            with open(self.ping_file, 'a') as fd:
-                for host in self.hosts:
+            # with open(self.ping_file, 'a') as fd:
+            for host in self.hosts:
+                with open(f'./data/{host}.txt', 'a') as fd:
                     command = ['ping', self.param, '5', f'{host}.com']
                     sites[host] = subprocess.run(command, capture_output=True)
                     fd.write('(\n{}\n{})\n'.format(time.strftime('%Y:%m:%d-%X-%w'),
-                                                   sites[host].stdout.decode('utf-8')))
+                                                    sites[host].stdout.decode('utf-8')))
         except Exception as e:
             print("This Exception -> ", e, " <- occurred.")
 
